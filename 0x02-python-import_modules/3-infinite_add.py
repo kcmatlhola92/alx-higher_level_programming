3357f276ae4a10b27d6735a9e2fdaf72ff6f14fa
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-12-07 07:40:07 -0800

    Python bytecode

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-     """Print the addition of all arguments."""
-     import sys
+    import sys
+    argc = len(sys.argv)
+    argv = sys.argv
+    res = 0
 
-     total = 0
-     for i in range(len(sys.argv) - 1):
-         total += int(sys.argv[i + 1])
-     print("{}".format(total))
+    for i in range(1, argc):
+        res = res + int(argv[i])
+
+    print(res)
