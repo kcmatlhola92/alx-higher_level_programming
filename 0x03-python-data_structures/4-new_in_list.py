aa5df662f5cd3ed9ffe4dc656d95d6ff0a0cec69
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-12-07 12:01:59 -0800

    Python - More Data Structures: Set, Dictionary

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    length = len(my_list)
-
-    new_list = my_list[:]
-
-    if 0 <= idx < length:
-        new_list[idx] = element
-
-        return (new_list)
+    if(my_list is not None):
+        m_list = my_list.copy()
+        if idx < 0 or idx >= len(my_list):
+            return my_list
+        else:
+            m_list[idx] = element
+            return m_list
