8727595f3822c3e0c2372e24ed8956e037874c2c
kcmatlhola92 <kcmatlhola@hotmail.com>
2022-12-20 07:27:43 -0800

    Python Classes

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -9,12 +9,7 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
 
-        Args:
-            size (int): The size of the new square.
-            position (int, int): The position of the new square.
-        """
-
-        Args:
+        args:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
         """
@@ -63,3 +58,14 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
+
+    def __str__(self):
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
