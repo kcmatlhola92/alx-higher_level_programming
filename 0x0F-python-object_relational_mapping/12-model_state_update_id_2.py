7cf9ab5a8c6bf65366cf450db64a817fe9852278
Keaobaka Matlhola <111046000+kcmatlhola92@users.noreply.github.com>
2023-03-20 17:09:01 +0200

    mapping

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""This script will list all states in database"""
-from sqlalchemy import create_engine
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).get(2)
-    state.name = "New Mexico"
+    new_instance = session.query(State).filter_by(id=2).first()
+    new_instance.name = 'New Mexico'
     session.commit()
-    session.close()
