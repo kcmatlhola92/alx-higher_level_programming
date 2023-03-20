a00a9783a2a19ffbf5514d1e5d8fef94bbc33953
Keaobaka Matlhola <111046000+kcmatlhola92@users.noreply.github.com>
2023-03-20 16:44:02 +0200

    mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Takes in an argument and displays all values in the states table of
-   hbtn_0e_0_usa where name matches the argument
-"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-                 ORDER BY states.id ASC".format(sys.argv[4]))
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
     rows = cur.fetchall()
-    for r in rows:
-        print(r)
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
