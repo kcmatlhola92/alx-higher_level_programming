c76dab369f36b232cd43588510ea7950ae61ee2b
Keaobaka Matlhola <111046000+kcmatlhola92@users.noreply.github.com>
2023-03-24 15:18:45 +0200

    mapping

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Start link class to table in database
 """
+Start link class to table in database
+"""
+
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
