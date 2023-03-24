3828a416ee1be8f9de6f88b4c58d3e538db07bcc
Keaobaka Matlhola <111046000+kcmatlhola92@users.noreply.github.com>
2023-03-24 15:28:41 +0200

    mapping

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
 """
+prints the State object with the name passed as argument from the database
+"""
+
 import sys
 from model_state import Base, State
 from model_city import City
