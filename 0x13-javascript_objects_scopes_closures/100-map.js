8fd89ba92aa1070686d6581a108ab691620723fe
Keaobaka Matlhola <111046000+kcmatlhola92@users.noreply.github.com>
2023-03-31 12:16:45 +0200

    js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+
+const list = require('./100-data').list;
+
+const newList = list.map((val, i) => val * i);
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(newList);
