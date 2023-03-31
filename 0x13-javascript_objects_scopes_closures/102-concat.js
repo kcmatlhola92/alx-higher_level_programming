89c1445e57653c3951c0215d90abc0f2a70cc4c8
Keaobaka Matlhola <111046000+kcmatlhola92@users.noreply.github.com>
2023-03-31 12:18:36 +0200

    js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
 const fs = require('fs');
-
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+const textA = fs.readFileSync(fileA, 'utf8');
+const textB = fs.readFileSync(fileB, 'utf8');
+fs.writeFileSync(fileC, textA + textB);
