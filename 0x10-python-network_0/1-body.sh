27ed91b0269e921485753baca46e570f24155501
Keaobaka Matlhola <111046000+kcmatlhola92@users.noreply.github.com>
2023-03-31 13:35:26 +0200

    Network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+#Takes in a URL, GET request to that URL, and displays the response body
+curl -Ls "$1"
